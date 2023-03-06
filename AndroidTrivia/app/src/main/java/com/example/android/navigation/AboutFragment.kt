49a98c11c79f93425e2e6cbd6a578d958c863ae4
marcellajinx <cellakomunita@gmail.com>
2023-03-06 11:09:40 +0700

    codelab: add navigation

--- AndroidTrivia/app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ AndroidTrivia/app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -17,25 +17,15 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import com.example.android.navigation.databinding.FragmentGameWonBinding
 
-
-class GameWonFragment : Fragment() {
+class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
-
-        binding.nextMatchButton.setOnClickListener { view : View ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
-        }
-        return binding.root
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
